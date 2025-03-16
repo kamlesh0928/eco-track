cfc4194f074587a83cb6eb535d213803dc0214b9
kamleshprajapati0928 <kamleshprajapati0928@gmail.com>
2025-03-16 10:40:56 +0000

    Added backend to get weather data of a city

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
-require('dotenv').config();
-
 const express = require('express');
+const cors = require("cors");
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const weatherRoutes = require('./routes/weatherRoutes');
+
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("I am the Root");
 });
 
+app.use('/api', weatherRoutes);
+
+const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on https://localhost:${PORT}`);
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
