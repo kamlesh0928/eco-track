879f736e71d619ee5a02c1fe364609dddec620d6
kamleshprajapati0928 <kamleshprajapati0928@gmail.com>
2025-03-31 16:58:14 +0000

    Python AI model is wroking now

--- ai-engine/flake.nix
+++ ai-engine/flake.nix
@@ -14,6 +14,10 @@
         pkgs.python311
         pkgs.python311Packages.pip
         pkgs.python311Packages.flask
+        pkgs.python311Packages.aiohttp-cors
+        pkgs.python311Packages.google-generativeai
+        pkgs.python311Packages.flask-cors
+        pkgs.python311Packages.python-dotenv
       ];
     };
   };
